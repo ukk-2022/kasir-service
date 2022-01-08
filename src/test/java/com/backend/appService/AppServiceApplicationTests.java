c84976490b28c3480f3c89efcd9126704664cc5d
Hafizh Kamus <kam.argana10@gmail.com>
2022-01-08 22:20:20 +0700

    template application spring 2.5.8 postgresql and swagger main configuration

---
+++ src/test/java/com/backend/appService/AppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.appService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
