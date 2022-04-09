ebedf5b949bd318f06a7b002bd5e6236bbed3a72
hafizhkamus <brilliant.lucky08@gmail.com>
2022-04-09 15:29:56 +0700

    update

--- src/main/java/com/backend/appService/controller/MenuController.java
+++ src/main/java/com/backend/appService/controller/MenuController.java
@@ -3,14 +3,20 @@ package com.backend.appService.controller;
 import com.backend.appService.entity.Menu;
 import com.backend.appService.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping(path = "/api/menu")
@@ -66,4 +72,27 @@ public class MenuController {
         }
     }
 
+    @PostMapping("/filesupload")
+    public ResponseEntity<Map<String, Object>> uploadFile(@RequestParam("file") MultipartFile file) {
+        String message = null;
+        Map<String, Object> pesan = new HashMap<>();
+        try {
+            String namaFile = service.uploadFile(file);
+            pesan.put("file", namaFile);
+            return ResponseEntity.ok().body(pesan);
+        } catch (Exception exception) {
+            pesan.put("pesan", "cannot input file");
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(pesan);
+        }
+    }
+
+    @GetMapping(value = "/file/{id}")
+    public ResponseEntity<InputStreamResource>getImage(@PathVariable("id") String id){
+        try{
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(
+                    new InputStreamResource( service.load(id).getInputStream() ));
+        }catch(IOException ex){
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(null);
+        }
+    }
 }
--- src/main/java/com/backend/appService/service/MenuService.java
+++ src/main/java/com/backend/appService/service/MenuService.java
@@ -3,10 +3,21 @@ package com.backend.appService.service;
 import com.backend.appService.dao.MenuDAO;
 import com.backend.appService.entity.Menu;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class MenuService {
@@ -14,8 +25,13 @@ public class MenuService {
     @Autowired
     private MenuDAO dao;
 
+    @Value("${file.path.image}")
+    private String basePath;
+
     public void save(Menu menu)throws SQLException{
         dao.save(menu);
+        Optional<Menu> data = dao.findLastTransaksi();
+        menu.setId(data.get().getId());
         dao.newRepo(menu);
     }
 
@@ -33,6 +49,35 @@ public class MenuService {
 
     public void delete(Integer id){
         dao.delete(id);
+    }
+
+    public String uploadFile(MultipartFile file){
+        try{
+            Path root = Paths.get(basePath);
+            String[] fileFrags = file.getOriginalFilename().split("\\.");
+            String extension = fileFrags[fileFrags.length - 1];
+            String uuid = UUID.randomUUID().toString() + "." + extension;
+            Files.copy(file.getInputStream(), root.resolve(uuid));
+            return uuid;
+        } catch (IOException e){
+            throw new RuntimeException("could not store the file. error : " + e.getMessage());
+        }
+    }
+
+    public Resource load(String fileName){
+        try{
+            Path root = Paths.get(basePath);
+            Path file = root.resolve(fileName);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return  resource;
+            } else {
+                throw  new RuntimeException("couldn't found the file");
+            }
+        } catch (MalformedURLException a){
+            throw  new RuntimeException("Cannot show picture");
+        }
     }
 
 }
